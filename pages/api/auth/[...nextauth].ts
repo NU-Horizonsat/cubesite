49bdc1d39a186376e6c4c4bf562bd01e74ab8e50
Madhav kapa <madhavkapa16@gmail.com>
2023-04-08 19:24:06 -0400

    auth update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,6 +11,7 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET as string,
 };
 
 export default NextAuth(authOptions);
